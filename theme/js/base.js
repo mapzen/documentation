a196c33ba756d08f820f6e6a76d9ca1604ec0f15
Lou Huang <lou@louhuang.com>
2015-08-19 17:22:09 -0400

    Put back affix for TOC

---
+++ theme/js/base.js
@@ -0,0 +1,56 @@
+
+function getSearchTerm()
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == 'q')
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+$(document).ready(function () {
+  var search_term = getSearchTerm()
+  var $search_modal = $('#mkdocs_search_modal')
+  var $docContainer = $('.documentation-container')
+  var $nav = $('nav')
+
+  if (search_term) {
+    $('#mkdocs-search-query').val(search_term)
+  }
+
+  // Autofocus to search.
+  $('#mkdocs-search-query').focus()
+
+  // Highlight.js
+  hljs.initHighlightingOnLoad();
+  $('table').addClass('table table-striped table-hover');
+
+  // Affix for side nav bar
+  $('#toc').affix({
+    offset: {
+      top: function () {
+        // Calculate the top offset
+        return $docContainer.offset().top - $nav.height()
+      },
+      bottom: function () {
+        return $('footer').outerHeight(true) + $('.documentation-meta').outerHeight(true)
+      }
+    }
+  })
+
+});
+
+
+$('body').scrollspy({
+    target: '.bs-sidebar',
+});
+
+/* Prevent disabled links from causing a page reload */
+$("li.disabled a").click(function() {
+    event.preventDefault();
+});
