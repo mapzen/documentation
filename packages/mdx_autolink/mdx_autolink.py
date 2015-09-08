4247e33f4f027031db6018a03d74ef9af13002ae
Lou Huang <lou@louhuang.com>
2015-09-08 17:23:34 -0400

    Update autolink extension hopefully it works

--- packages/mdx_autolink/mdx_autolink.py
+++ packages/mdx_autolink/mdx_autolink.py
@@ -11,36 +11,21 @@ Initial goal is to mimic GitHub-flavored Markdown:
 Copyright 2015 Lou Huang
 
 License: [MIT](http://opensource.org/licenses/MIT)
-
-from __future__ import absolute_import
-from __future__ import unicode_literals
-
-from markdown.inlinepatterns import SubstituteTagPattern
-from markdown.postprocessors import Postprocessor
-from markdown import Extension
-
-
-BR_RE = r'\n'
-
-
-class AutolinkExtension(Extension):
-
-    def extendMarkdown(self, md, md_globals):
-        br_tag = SubstituteTagPattern(BR_RE, 'br')
-        md.inlinePatterns.add('nl', br_tag, '_end')
-
-
-def makeExtension(*args, **kwargs):
-    return AutolinkExtension(*args, **kwargs)
 """
 
 import re
 from markdown.preprocessors import Preprocessor
 from markdown.extensions import Extension
 
+"""
+# Old regex is really long and caused problems
+# Retained here for reference
 urlfinder = re.compile(r'((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+(:[0-9]+)?|'
                        r'(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:/[\+~%/\.\w\-_]*)?\??'
                        r'(?:[\-\+=&;%@\.\w_]*)#?(?:[\.!/\\\w]*))?)')
+"""
+
+urlfinder = re.compile(r'(https?:\/\/([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)')
 
 # TODO: Terminate the URL regex if there is a period and immediately after is whitespace or linebreak.
 
