422e0f5ac3a84681eb4b840853247e604520414d
Lou Huang <lou@louhuang.com>
2017-06-12 16:16:17 -0400

    add api key swap module

---
+++ theme/js/apikeys.js
@@ -0,0 +1,26 @@
+// your-mapzen-api-key
+(function () {
+  var placeholder = 'your-mapzen-api-key';
+  var el = document.querySelector('.documentation-content');
+  if (!el) return;
+
+  // window.fetch('/api/keys.json', { credentials: 'same-origin '})
+    // .then(function (response) {
+    //   return response.json();
+    // })
+  // TEMPORARY: Use a placeholder key; don't actually fetch
+  Promise.resolve([{key: 'test-1234567'}])
+    .then(function (data) {
+      // Silent fail if data is not an array, or is zero-length
+      if (!Array.isArray(data) || data.length === 0) return;
+      searchAndReplaceWithKey(el, data[0].key);
+    })
+    .catch(function (err) {
+      console.error('Error fetching user API keys: ', err);
+    });
+
+  function searchAndReplaceWithKey(el, key) {
+    var regex = new RegExp(placeholder, 'g');
+    el.innerHTML = el.innerHTML.replace(regex, key);
+  }
+})();
