98ec92b5b245416d92c3e0bb6ba673c89556703f
Lou Huang <lou@louhuang.com>
2017-06-23 13:29:23 -0700

    xhr fallback

--- theme/js/apikeys.js
+++ theme/js/apikeys.js
@@ -3,18 +3,56 @@
   var el = document.querySelector('.documentation-content');
   if (!el) return;
 
-  window.fetch('/api/keys.json', { credentials: 'same-origin' })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      // Silent fail if data is not an array, or is zero-length
-      if (!Array.isArray(data) || data.length === 0) return;
-      searchAndReplaceWithKey(el, data[0].key);
-    })
-    .catch(function (err) {
-      console.error('Error fetching user API keys: ', err);
-    });
+  // Polyfill isArray
+  if (!Array.isArray) {
+    Array.isArray = function(arg) {
+      return Object.prototype.toString.call(arg) === '[object Array]';
+    };
+  }
+
+  // Use fetch if we have it; otherwise use XHR until fetch is polyfilled for old browsers
+  if (window.fetch) {
+    window.fetch('/api/keys.json', { credentials: 'same-origin' })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(processKeys)
+      .catch(function (err) {
+        console.error('Error fetching user API keys: ', err);
+      });
+  } else {
+    var request = new XMLHttpRequest();
+
+    request.open('GET', '/api/keys.json');
+    request.onerror = function (err) {
+      throwError(err);
+    };
+    request.onreadystatechange = function () {
+      if (request.readyState === 4) {
+        if (request.status === 200 || request.status === 304) {
+          try {
+            var response = JSON.parse(request.responseText);
+            processKeys(response);
+          } catch (err) {
+            throwError(err);
+          }
+        } else {
+          throwError(request.status);
+        }
+      }
+    };
+    request.send();
+  }
+
+  function throwError(err) {
+    console.error('Error fetching user API keys: ', err);
+  }
+
+  function processKeys(data) {
+    // Silent fail if data is not an array, or is zero-length
+    if (!Array.isArray(data) || data.length === 0) return;
+    searchAndReplaceWithKey(el, data[0].key);
+  }
 
   function searchAndReplaceWithKey(el, key) {
     var regex = new RegExp(placeholder, 'g');
